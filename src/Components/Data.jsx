5da94e2c23ad16228e9bbab2e4df49b87951d637
abir718 <abir.hasan2003s@gmail.com>
2024-11-05 10:08:50 +0600

    images and adjustment to home content added

--- src/Components/Data.jsx
+++ src/Components/Data.jsx
@@ -1,11 +1,15 @@
 
 const Data = ({allData}) => {
-    let {product_title , price} = allData
+    let {product_title , price , product_image} = allData
     return (
-        <div>
-            <p>{product_title}</p>
-            <p>{price}</p>
-            <button className="border-2 py-1 px-3 rounded-full text-[rgb(149,56,226)] border-[rgb(149,56,226)]">View Details</button>
+        <div className="border-2 rounded-lg p-4 bg-white w-[300px]">
+            <div className="flex items-center justify-center">
+            <img className="h-52 w-fit rounded-lg" src={product_image} alt="" />
+            </div>
+
+            <p className="text-xl font-bold my-2">{product_title}</p>
+            <p className="my-2 font-medium text-gray-500">{price}</p>
+            <button className="border-2 my-2 py-1 px-3 rounded-full text-[rgb(149,56,226)] border-[rgb(149,56,226)]">View Details</button>
         </div>
     );
 };
