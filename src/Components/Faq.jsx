83d5a1bff5fc748c93332b4b8fe95de745651971
abir718 <abir.hasan2003s@gmail.com>
2024-11-06 23:37:12 +0600

    faq helmet added

--- src/Components/Faq.jsx
+++ src/Components/Faq.jsx
@@ -1,7 +1,11 @@
+import { Helmet } from "react-helmet";
 
 const Faq = () => {
     return (
         <div>
+        <Helmet>
+        <title>FAQ | Gadget Heaven</title>
+        </Helmet>  
             <div className="bg-purple-600">
                 <h1 className="text-3xl font-bold text-white text-center w-[900px] mx-auto pt-10">FAQ</h1>
                 <p className="text-white text-center w-[900px] mx-auto py-4 ">Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!</p>
--- src/Header.jsx
+++ src/Header.jsx
@@ -18,8 +18,8 @@ const Header = () => {
                 <NavLink className={({ isActive }) => `font-medium ${isActive ? 'text-purple-700 underline' : ''}`} to='/faq'>FAQ</NavLink>
             </div>
             <div className="flex items-center gap-6">
-                <NavLink><IoCartOutline className="size-10 border-2 p-2 rounded-full text-black bg-white" /></NavLink>
-                <NavLink><FaRegHeart className="size-10 border-2 p-2 rounded-full text-black bg-white"/></NavLink>
+                <IoCartOutline className="size-10 border-2 p-2 rounded-full text-black bg-white" />
+                <FaRegHeart className="size-10 border-2 p-2 rounded-full text-black bg-white"/>
             </div>
         </div>
         </section>
